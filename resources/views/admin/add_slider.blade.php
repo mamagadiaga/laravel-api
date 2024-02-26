06eeb49490a98a181b9d8016cd7f08ae0c5a1124
Mama GDG <mamsogdg@gmail.com>
2024-02-26 18:33:16 +0000

    check

--- resources/views/admin/add_slider.blade.php
+++ resources/views/admin/add_slider.blade.php
@@ -4,20 +4,20 @@
 				<li>
 					<i class="icon-home"></i>
 					<a href="index.html">Home</a>
-					<i class="icon-angle-right"></i> 
+					<i class="icon-angle-right"></i>
 			    </li>
 			    <li>
 					<i class="icon-edit"></i>
 					<a href="#">Add Slider</a>
 				</li>
 			</ul>
-			
+
 			<div class="row-fluid sortable">
 				<div class="box span12">
 					<div class="box-header" data-original-title>
 						<h2><i class="halflings-icon edit"></i><span class="break"></span>Add Slider</h2>
 					</div>
-					<div class="box-content">                   
+					<div class="box-content">
                          <p class="alert-success">
 							<?php
 							$message = Session::get('message');
@@ -27,9 +27,9 @@
 							}
 						?>
 						</p>
-                 
+
 						<form class="form-horizontal" action="{{URL::to('/save-slider')}}" method="POST" enctype="multipart/form-data">
-							{{ csrf_field() }} 
+							{{ csrf_field() }}
 						  <fieldset>
                             <div class="control-group">
 							  <label class="control-label" for="fileInput">Slider Image</label>
@@ -42,15 +42,15 @@
 							  <div class="controls">
 								<input class="input-file uniform_on" name="publication_status" value="1" type="checkbox" required="" >
 							  </div>
-							</div>  
+							</div>
 
 							<div class="form-actions">
 							  <button type="submit" class="btn btn-primary">Add Slider</button>
 							  <button type="reset" class="btn">Cancel</button>
 							</div>
 						  </fieldset>
-						</form>   
+						</form>
 					</div>
-				</div><!--/span-->
-			</div><!--/row-->
+				</div>
+			</div>
 @endsection
